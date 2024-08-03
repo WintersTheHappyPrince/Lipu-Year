8aadafd7fb5f007dcdf2c76f72cacc0a71f5f042
WintersTheHappyPrince <Guanglin02@outlook.com>
2024-08-03 17:07:26 +0800

    开始测试

---
+++ Assets/Void.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Void : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            player.Die();
+        }
+    }
+}
